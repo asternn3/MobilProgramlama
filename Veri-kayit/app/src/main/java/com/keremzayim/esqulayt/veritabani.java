c2240cb68a0d2200ba3d23d98ff7b4f245742a7c
asternn3 <erdemgokh@gmail.com>
2026-05-08 02:12:06 +0300

    Veri kaydetme uygulaması

---
+++ Veri-kayit/app/src/main/java/com/keremzayim/esqulayt/veritabani.java
@@ -0,0 +1,32 @@
+package com.keremzayim.esqulayt;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class veritabani extends SQLiteOpenHelper {
+    private static final String VERITABANI_ADI = "Ogrenciler.db";
+    private static final int SURUM = 1;
+
+    public veritabani(@Nullable Context context) {
+        super(context, VERITABANI_ADI, null, SURUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE IF NOT EXISTS OgrenciBilgi (" +
+                "ad TEXT NOT NULL, " +
+                "soyad TEXT NOT NULL, " +
+                "yas INTEGER NOT NULL, " +
+                "sehir TEXT NOT NULL)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS OgrenciBilgi");
+        onCreate(db);
+    }
+}
